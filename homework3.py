0ab91e2aff55b54cdf1c0befbfdc7e8bbcb5f264
Anton801106 <listboxx@yandex.ru>
2024-07-17 11:58:23 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,14 @@
+#Динамическая типизация
+
+name = 'Anton'
+print(name)
+age = 43
+print(age)
+new_age = age + 1
+print(new_age)
+Is_student = True
+print('Name:' , name,)
+print('Age:', age)
+print('New age:', new_age)
+print('Is student:', Is_student)
+
